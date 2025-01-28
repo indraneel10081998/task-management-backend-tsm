be54a399aa7674009e367706a689fae1e67c074d
indraneel10081998 <83970865+indraneel10081998@users.noreply.github.com>
2025-01-28 13:00:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://task-management-frontend-tsm.onrender.com/",
 }));
 
 
